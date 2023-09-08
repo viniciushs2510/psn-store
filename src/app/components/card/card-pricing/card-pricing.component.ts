27ccafaf965506dbba9a882ef1f58d315c53844f
Vinicius Honorato <viniciushs2510@gmail.com>
2023-09-08 10:12:13 -0300

    finish project

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-pricing.component.css']
 })
 export class CardPricingComponent implements OnInit {
+
+  @Input()
+  gameType: string = ""
+
+  @Input()
+  gamePrice: string = ""
 
   constructor() { }
 
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input()
+  gameCover: string = "";
+
+  @Input()
+  gameLabel = '';
+
+  @Input()
+  gameType: string = ""
+
+  @Input()
+  gamePrice: string = ""
 
   constructor() { }
 
